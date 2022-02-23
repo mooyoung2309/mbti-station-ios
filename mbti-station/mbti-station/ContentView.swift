f1eb20b0ed931547b2cfefbe3aeed2eb1dc60e18
송영모 <mooyoung2309@gmail.com>
2022-02-23 19:57:46 +0900

    feat: start project

---
+++ mbti-station/mbti-station/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mbti-station
+//
+//  Created by 송영모 on 2022/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello !")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mbti-station/mbti-station/mbti_stationApp.swift
@@ -0,0 +1,17 @@
+//
+//  mbti_stationApp.swift
+//  mbti-station
+//
+//  Created by 송영모 on 2022/02/23.
+//
+
+import SwiftUI
+
+@main
+struct mbti_stationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
